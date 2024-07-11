2905ce5aeefa4f5f5f24d8f73165a7f1f32db868
TevinDeja <tevinowena@gmail.com>
2024-07-11 19:46:39 +0000

    Safe divide

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,2 @@
 -- Task: Create an index 'idx_name_first'
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
+CREATE INDEX idx_name_first ON names (name (1));
--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- Task: Create a composite index 'idx_name_first_score'
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name (1), score);
