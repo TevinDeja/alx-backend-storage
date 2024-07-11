76079828d4b0309aabfa5760fe3c62a9b575bd3f
TevinDeja <tevinowena@gmail.com>
2024-07-11 19:28:22 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,14 +1,13 @@
 -- Task: Create a trigger to decrease the quantity of an item
-
 DELIMITER //
 
-CREATE TRIGGER decrease_quantity_after_order
+CREATE TRIGGER decrease_item_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-   UPDATE items
-   SET quantity = quantity - NEW.number
-   WHERE item_id = NEW.item_id;
-END;//
+    UPDATE items
+    SET quantity = quantity - NEW.quantity
+    WHERE item_id = NEW.item_id;
+END//
 
 DELIMITER ;
