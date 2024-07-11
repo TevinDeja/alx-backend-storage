3973d586bca2e9df34971abbc10b2888ae4c6811
TevinDeja <tevinowena@gmail.com>
2024-07-11 08:49:08 +0000

    Safe divide

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,3 @@
+-- Task: Create an index 'idx_name_first'
+
+CREATE INDEX idx_name_first ON names (LEFT(name, 1));
---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,3 @@
+-- Task: Create a composite index 'idx_name_first_score'
+
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
