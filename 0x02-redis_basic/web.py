33a0bd59dda55d9dfcbe8552c6e50c93c531e30b
TevinDeja <tevinowena@gmail.com>
2024-07-19 10:31:00 +0000

    web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,29 +6,31 @@ Module for fetching web pages with caching and access counting.
 import redis
 import requests
 from functools import wraps
-from typing import Callable
 
 redis_store = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
+def data_cacher(method)
     """
     Decorator to cache the result of a function and track how many times it's called.
     """
     @wraps(method)
-    def wrapper(url: str) -> str:
+    def wrapper(url)
         """
         The wrapper function for caching the output and tracking access.
         """
-        redis_store.incr(f'count:{url}')
+        cached_key = "cached:" + url
+        cached_result = redis_store.get(cached_key)
         
-        cached_result = redis_store.get(f'result:{url}')
         if cached_result:
             return cached_result.decode('utf-8')
         
+        count_key = "count:" + url
         result = method(url)
-        
-        redis_store.setex(f'result:{url}', 10, result)
+
+        redis_store.incr(count_key)
+        redis_store.set(cached_key, result)
+        redis_store.expire(cached_key, 10)
         
         return result
     return wrapper
